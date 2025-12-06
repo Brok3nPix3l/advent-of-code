41dc0d1856f381cf10506ac57d7c6a73d0bfcc16
Brok3nPix3l <rjbabala@yahoo.com>
2025-12-06 15:48:51 -0500

    day 6 part 2

--- src/2025/Java/Days/Day6.java
+++ src/2025/Java/Days/Day6.java
@@ -2,10 +2,7 @@ package Days;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Day6 implements DailyChallenge {
     File inputFile;
@@ -17,7 +14,8 @@ public class Day6 implements DailyChallenge {
 
     // split each line on whitespace (trim leading and trailing whitespace)
     // check for a "+" or "*" to decide if we're at the end of the file
-    // iterate through each index (problem) and perform the current operation to those numbers, then add to the grand total
+    // iterate through each index (problem) and perform the current operation to those numbers, then add to the
+    // grand total
 
     // 1 2 3
     // 4 5 6
@@ -63,13 +61,68 @@ public class Day6 implements DailyChallenge {
         return ans;
     }
 
+    // 123 328
+    //  45 64
+    //   6 98
+    // *   +
+    // [[[1],[2,4],[3,5,6]],[[3,6,9],[2,4,8],[8]]]
+
+    // since the operators are always aligned with the first column (number) of a problem, can use that to
+    // determine where the breaks are
     public long Part2(boolean debug) {
-        throw new RuntimeException("Not implemented yet");
-//        try (Scanner scanner = new Scanner(this.inputFile)) {
-//            while (scanner.hasNextLine()) {
-//            }
-//        } catch (FileNotFoundException e) {
-//            throw new RuntimeException(e);
-//        }
+        long ans = 0L;
+        try (Scanner scanner = new Scanner(this.inputFile)) {
+            Map<Integer, Integer> digitsByColumnMap = new HashMap<>();
+            int maxColumn = 0;
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.startsWith("*") || line.startsWith("+")) {
+                    char curOperator = line.charAt(0);
+                    long curProblemAns = 0;
+                    for (int i = 0; i < maxColumn; i++) {
+                        if (i < line.length() && line.charAt(i) != ' ') {
+                            ans += curProblemAns;
+                            if (debug) {
+                                System.out.println("curProblemAns=" + curProblemAns);
+                            }
+                            curOperator = line.charAt(i);
+                            curProblemAns = curOperator == '+' ? 0L : 1L;
+                        }
+                        if (digitsByColumnMap.containsKey(i)) {
+                            switch (curOperator) {
+                                case '+':
+                                    curProblemAns += digitsByColumnMap.get(i);
+                                    if (debug) {
+                                        System.out.println("adding " + digitsByColumnMap.get(i));
+                                    }
+                                    break;
+                                case '*':
+                                    curProblemAns *= digitsByColumnMap.get(i);
+                                    if (debug) {
+                                        System.out.println("multiplying " + digitsByColumnMap.get(i));
+                                    }
+                            }
+                        }
+                    }
+                    ans += curProblemAns;
+                    if (debug) {
+                        System.out.println("curProblemAns=" + curProblemAns);
+                    }
+                }
+                for (int i = 0; i < line.length(); i++) {
+                    if (line.charAt(i) == ' ') {
+                        continue;
+                    }
+                    digitsByColumnMap.put(i, digitsByColumnMap.getOrDefault(i, 0) * 10 + line.charAt(i) - '0');
+                }
+                maxColumn = Math.max(maxColumn, line.length());
+            }
+            if (debug) {
+                System.out.println(digitsByColumnMap);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return ans;
     }
 }
