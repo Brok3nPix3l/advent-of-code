ff12d78e9b2bae1479a93ed78e4c66b4b2644019
BABA9302 <baba9302@wsfsfc.net>
2025-12-11 14:01:34 -0500

    reverted runtime version in pom to 17

---
+++ src/2025/Java/Days/Day11.java
@@ -0,0 +1,34 @@
+package Days;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day11 implements DailyChallenge {
+    File inputFile;
+
+    public Day11(File inputFile) throws FileNotFoundException {
+        new Scanner(inputFile);
+        this.inputFile = inputFile;
+    }
+
+    public long Part1(boolean debug) {
+        throw new RuntimeException("Not implemented yet");
+//        try (Scanner scanner = new Scanner(this.inputFile)) {
+//            while (scanner.hasNextLine()) {
+//            }
+//        } catch (FileNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
+    }
+
+    public long Part2(boolean debug) {
+        throw new RuntimeException("Not implemented yet");
+//        try (Scanner scanner = new Scanner(this.inputFile)) {
+//            while (scanner.hasNextLine()) {
+//            }
+//        } catch (FileNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
+    }
+}
