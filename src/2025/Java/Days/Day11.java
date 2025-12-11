3dd4d82d4306d41935cb3ac0be6cd83e3271409f
BABA9302 <baba9302@wsfsfc.net>
2025-12-11 14:11:26 -0500

    day 11 part 1

--- src/2025/Java/Days/Day11.java
+++ src/2025/Java/Days/Day11.java
@@ -2,7 +2,7 @@ package Days;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Day11 implements DailyChallenge {
     File inputFile;
@@ -13,13 +13,33 @@ public class Day11 implements DailyChallenge {
     }
 
     public long Part1(boolean debug) {
-        throw new RuntimeException("Not implemented yet");
-//        try (Scanner scanner = new Scanner(this.inputFile)) {
-//            while (scanner.hasNextLine()) {
-//            }
-//        } catch (FileNotFoundException e) {
-//            throw new RuntimeException(e);
-//        }
+        long ans = 0L;
+        try (Scanner scanner = new Scanner(this.inputFile)) {
+            Map<String, List<String>> cableMap = new HashMap<>();
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] tokens = line.split(" ");
+                String sourceDevice = tokens[0].substring(0, tokens[0].length()-1); // remove trailing ":"
+                List<String> destinationDevices = new ArrayList<>(Arrays.asList(tokens).subList(1, tokens.length));
+                cableMap.put(sourceDevice, destinationDevices);
+            }
+            if (debug) {
+                System.out.println("cableMap: " + cableMap);
+            }
+            Queue<String> queue = new LinkedList<>();
+            queue.add("you");
+            while (!queue.isEmpty()) {
+                String currentDevice = queue.poll();
+                if (currentDevice.equals("out")) {
+                    ans++;
+                    continue;
+                }
+                queue.addAll(cableMap.get(currentDevice));
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return ans;
     }
 
     public long Part2(boolean debug) {
