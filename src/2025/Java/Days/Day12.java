c6a6b551c8bcc5524553e0136e2be9e28af5a359
Brok3nPix3l <rjbabala@yahoo.com>
2025-12-12 10:59:48 -0500

    day 12 part 1 wip
    updated day template

--- src/2025/Java/Days/Day12.java
+++ src/2025/Java/Days/Day12.java
@@ -4,35 +4,34 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class DayTemplate implements DailyChallenge {
+public class Day12 implements DailyChallenge {
     File inputFile;
 
-    public DayTemplate(File inputFile) throws FileNotFoundException {
+    public Day12(File inputFile) throws FileNotFoundException {
         new Scanner(inputFile);
         this.inputFile = inputFile;
     }
 
     public long Part1(boolean debug) {
-        throw new RuntimeException("Not implemented yet");
-//        long ans = 0L;
-//        try (Scanner scanner = new Scanner(this.inputFile)) {
-//            while (scanner.hasNextLine()) {
-//            }
-//        } catch (FileNotFoundException e) {
-//            throw new RuntimeException(e);
-//        }
-//        return ans;
+        long ans = 0L;
+        try (Scanner scanner = new Scanner(this.inputFile)) {
+            boolean readingPresents = true;
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return ans;
     }
 
     public long Part2(boolean debug) {
         throw new RuntimeException("Not implemented yet");
-//        long ans = 0L;
 //        try (Scanner scanner = new Scanner(this.inputFile)) {
 //            while (scanner.hasNextLine()) {
 //            }
 //        } catch (FileNotFoundException e) {
 //            throw new RuntimeException(e);
 //        }
-//        return ans;
     }
 }
